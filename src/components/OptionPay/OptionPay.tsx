dbc08dcca969b7a9cf06fcb5f34f5883c0df252d
Ivan Hurtado <ivan.hurtado92@outlook.com>
2025-03-21 19:44:31 -0600

    feat: :sparkles: Componentes de Opciones de Compra Iniciados

--- src/components/OptionPay/OptionPay.tsx
+++ src/components/OptionPay/OptionPay.tsx
@@ -1,9 +1,21 @@
 import './OptionPay.css'
+import '../../models/SubCost'
+import { SubCost } from '../../models/SubCost'
 
-export function OptionPay(){
+export function OptionPay({SubType,Cost}:SubCost){
     return(
-        <div className='Option'>
-
+        <div className='grid'>
+            <div className='left'>
+                <span className='dot'></span>
+            </div>
+            <div className='right'>
+                <div className='half'>
+                    <h4>Pay {SubType}</h4>
+                </div>
+                <div className='half'>
+                    <h5>${Cost}/Month/Member</h5>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/PaymentOption/PaymentOptions.tsx
+++ src/components/PaymentOption/PaymentOptions.tsx
@@ -1,10 +1,16 @@
 import "./PaymentOptions.css"
+import { OptionPay } from "../OptionPay/OptionPay"
 
 export function PaymentOptions(){
     return(
         <div className="select-plan">
-            <h3>Starter Plan</h3>
-            <h4>Total</h4>
+            <h2 className="title">Starter Plan</h2>
+            <OptionPay/>
+            <hr className="separador"/>
+            <div className="total">
+                <h3 className="left">Total</h3>
+                <h3 className="right">100 / Month</h3>
+            </div>
         </div>
     )
 }
